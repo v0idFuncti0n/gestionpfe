7c1342bdf45e60b79da1f5c9a02d1bb42784849b
Anas <lakchouchanas@gmail.com>
2023-01-29 19:44:49 +0100

    add roles to AppUserResponse

--- src/main/java/com/gestionpfe/model/responses/AppUserResponse.java
+++ src/main/java/com/gestionpfe/model/responses/AppUserResponse.java
@@ -1,9 +1,14 @@
 package com.gestionpfe.model.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gestionpfe.enums.AppUserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Getter
 @Setter
@@ -16,4 +21,7 @@ public class AppUserResponse {
     private String email;
     private String password;
     private String accessToken;
+    @Enumerated(EnumType.STRING)
+    @JsonProperty("roles")
+    private AppUserRole appUserRole;
 }
