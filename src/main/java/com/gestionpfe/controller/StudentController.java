810e819d05258990df377399a950ca7b347fa78b
Anas <lakchouchanas@gmail.com>
2023-02-11 17:34:39 +0100

    add findByStudentId

--- src/main/java/com/gestionpfe/controller/StudentController.java
+++ src/main/java/com/gestionpfe/controller/StudentController.java
@@ -1,6 +1,14 @@
 package com.gestionpfe.controller;
 
+import com.gestionpfe.model.AppUser;
+import com.gestionpfe.model.responses.StudentResponse;
+import com.gestionpfe.model.responses.SupervisorResponse;
+import com.gestionpfe.services.AppUserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/student")
 public class StudentController {
 
+    private final AppUserService appUserService;
+    public StudentController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
     @GetMapping("/welcome")
     public String getWelcomeMessage() {
         return "Welcome duude!";
+    }
+
+    @GetMapping(path = "/{student-id}")
+    public ResponseEntity<StudentResponse> findStudentById(@PathVariable("student-id") Long studentId) {
+        StudentResponse studentResponse = new StudentResponse();
+        AppUser student = appUserService.findById(studentId);
+        BeanUtils.copyProperties(student, studentResponse);
+
+        return new ResponseEntity<>(studentResponse, HttpStatus.OK);
     }
 }
